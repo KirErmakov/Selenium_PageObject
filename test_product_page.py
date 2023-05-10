318a096fb929dce522e931ae86322c4787cbb4d7
Kirill Ermakov <lvstrng161@gmail.com>
2023-05-10 14:46:59 +0400

    test_product_page.py update

---
+++ test_product_page.py
@@ -0,0 +1,39 @@
+import time
+from .pages.product_page import ProductPage
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.solve_quiz_and_get_code()
+    product_page.should_be_item_in_basket()
+    product_page.should_be_basket_total_equal_item_price()
+    time.sleep(3)
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link, 0)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link, 0)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link, 0)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.should_success_message_disappear()
+
+
+
+
+
+
